8800dc6860af47a35b3b7ebcc5a7a72849f1f152
catmeme6 <107052782+catmeme6@users.noreply.github.com>
2022-06-11 13:18:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let text = document.getElementById('text');
+let hr = document.getElementById('hr');
+function showData (event){
+var hr = document.createElement('hr');
+var element1 = document.createElement('p');
+var element2 = document.createElement('p');
+var Nik = document.getElementById("Ник").value
+var Comment = document.getElementById("Коментария").value
+element1.textContent = Nik;
+element2.textContent = Comment;
+text.append(element1, element2, hr);
+}
+
